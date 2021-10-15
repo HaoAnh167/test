8ab63c64a58f3260e66738fd54ca5573a6f3b671
HaoAnh167 <hao12471@gmail.com>
2021-10-15 09:50:11 +0700

    .

---
+++ Luv.js
@@ -0,0 +1,8 @@
+function Yes() {
+    alert("HAH, but i hate you :))");
+}
+
+function No() {
+   document.getElementById("No").style.top = Math.random() * 500 + "px";
+   document.getElementById("No").style.left = Math.random() * 500 + "px";
+}
